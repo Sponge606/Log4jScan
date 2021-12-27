6cea3e1286d5cd064e888b82426ae77b5eef19ff
Sponge606 <96730470+Sponge606@users.noreply.github.com>
2021-12-27 22:25:27 +0800

    Create CreateProcess.cpp

---
+++ CreateProcess.cpp
@@ -0,0 +1,21 @@
+#pragma once
+#include "stdafx.h"
+#include <Windows.h>
+#include <TlHelp32.h>
+#include <string>
+#include <atlstr.h>
+#include <vector>
+#include <ShlObj.h>
+#include <cstring>
+#include <Atlbase.h>
+#include <Shlwapi.h>
+#include <comdef.h>
+#include <taskschd.h>
+#include <gpedit.h>
+#include <map>
+#pragma comment(lib, "taskschd.lib")
+#pragma comment(lib, "Shlwapi.lib")
+#include <iostream>
+using namespace std;
+
+
